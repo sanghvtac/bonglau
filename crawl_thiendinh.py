8c93e5638348e1dc4bb87fc3f6d9ae7b0338295d
sanghvtac <sanghv.tac@gmail.com>
2026-04-11 18:54:24 +0700

    Update crawl_thiendinh.py

--- crawl_thiendinh.py
+++ crawl_thiendinh.py
@@ -10,7 +10,7 @@ from io import BytesIO
 from PIL import Image
 from playwright.async_api import async_playwright
 
-TARGET_URL    = "https://sv1.thiendinh.live/lich-thi-dau/bong-da?by=state&value=live"
+TARGET_URL    = "https://sv2.thiendinh1.live/lich-thi-dau/bong-da?by=state&value=live"
 COVER_IMAGE   = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsJSuLHF9klemrEvihd_wyrOc0DkdxHcS9Kw&s"
 GITHUB_REPO   = "sanghvtac/bonglau"
 GITHUB_BRANCH = "main"
